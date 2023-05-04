ca7a6f96042f3cb4fb19362249a5223a7333be85
Aditya Rathod <Aditya Rathod@LAPTOP-D5AI6NEC>
2023-05-04 10:14:46 +0530

    Add project files.

---
+++ Controllers/QuizsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuizWebApp.Data;
+using QuizWebApp.Models;
+
+namespace QuizWebApp.Controllers
+{
+    public class QuizsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QuizsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Quizs
+        public async Task<IActionResult> Index()
+        {
+              return _context.Quiz != null ? 
+                          View(await _context.Quiz.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.Quiz'  is null.");
+        }
+        // GET: Quizs/ShowSearchForm
+        public async Task<IActionResult> ShowSearchForm()
+        {
+            return _context.Quiz != null ?
+                        View() :
+                        Problem("Entity set 'ApplicationDbContext.Quiz'  is null.");
+        }
+
+        // GET: Quizs/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(String SearchPhrase)
+        {
+            return _context.Quiz != null ?
+                        View("Index",await _context.Quiz.Where(j=>j.QuizQuestion.Contains(SearchPhrase)).ToListAsync()) :
+                        Problem("Entity set 'ApplicationDbContext.Quiz'  is null.");
+        }
+        // GET: Quizs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Quiz == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+
+            return View(quiz);
+        }
+
+        // GET: Quizs/Create
+        [Authorize]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Quizs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,QuizQuestion,QuizAnswer")] Quiz quiz)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(quiz);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quiz);
+        }
+
+        // GET: Quizs/Edit/5
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Quiz == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz.FindAsync(id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+            return View(quiz);
+        }
+
+        // POST: Quizs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,QuizQuestion,QuizAnswer")] Quiz quiz)
+        {
+            if (id != quiz.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(quiz);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QuizExists(quiz.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(quiz);
+        }
+
+        // GET: Quizs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Quiz == null)
+            {
+                return NotFound();
+            }
+
+            var quiz = await _context.Quiz
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (quiz == null)
+            {
+                return NotFound();
+            }
+
+            return View(quiz);
+        }
+
+        // POST: Quizs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Quiz == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Quiz'  is null.");
+            }
+            var quiz = await _context.Quiz.FindAsync(id);
+            if (quiz != null)
+            {
+                _context.Quiz.Remove(quiz);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QuizExists(int id)
+        {
+          return (_context.Quiz?.Any(e => e.ID == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/Quiz.cs
@@ -0,0 +1,15 @@
+﻿namespace QuizWebApp.Models
+{
+    public class Quiz
+    {
+        public int ID { get; set; }
+        public string QuizQuestion { get; set; }
+        public string QuizAnswer { get; set;}
+
+        public Quiz()
+        {
+            
+        }
+
+    }
+}
